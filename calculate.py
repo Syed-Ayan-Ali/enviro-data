578de0b5b584a7372c640d27916409ab82694c66
DarthSpector <70070770+DarthSpector@users.noreply.github.com>
2021-07-23 07:58:49 +0500

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,54 +1,61 @@
-urls=['https://bit.ly/3eeuGuv','https://bit.ly/36B3GkG','https://bit.ly/3rcdkU7','https://bit.ly/36FZWy1','https://bit.ly/3ebFUzK','https://bit.ly/3xQucSM','https://bit.ly/3wAuWdr','https://bit.ly/3B4f3PO','https://bit.ly/36ARfoM','https://bit.ly/3r6TGsN','https://bit.ly/3i6adJf','https://bit.ly/3AWYAx7'
-,'https://bit.ly/3AXec3v','https://bit.ly/3efzuiW','https://bit.ly/3AVz23r','https://bit.ly/3efEx2Y','https://bit.ly/3xHQk1Y','https://bit.ly/2T9i0xD'] 
-
-
-Sector=['G-5','G-6','G-7','G-8','G-9','G-10','G-11','G-12','G-13','G-14','G-15','G-16'
-,'F-5','F-6','F-7','F-8','F-10','F-11']
-
-
+Sectors=['G-5','G-6','G-7', 'G-8', 'G-9', 'G-10', 'G-11','G-12','G-13', 'G-14', 'G-15', 'G-16'
+,'F-5','F-6','F-7','F-8','F-10','F-11'
+,'E-7','E-8','E-9','E-11'
+,'I-8','I-9','I-10','I-11'
+,'H-8','H-9','H-10','H-11']
 
 
 
-global name
-
-global data
-
-
-
-def screenshot(url,sectorInitall):
+def website_opener():
+    data=''    
+    global sector,name
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support import expected_conditions as EC
     from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
     from time import sleep
     from webdriver_manager.firefox import GeckoDriverManager
 
+
+
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    driver.get(url)
-    driver.fullscreen_window
-    sleep(0.35)
-    global name
+    driver.get("https://www.google.com/maps/place/G-5,+Islamabad,+Islamabad+Capital+Territory/@33.72064,73.0929157,16z/data=!4m5!3m4!1s0x38dfc07b1b153ac9:0x7a6fc13096543e55!8m2!3d33.7218048!4d73.0980773!5m2!1e4!1e1")
+    driver.maximize_window() 
+    # Inital google maps window --- opens with Traffic data already on
+    sleep(0.22)
+    for sector in Sectors: 
+        name=sector + '.png'
+
+        if sector != 'G-5':
+            WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.ID, "searchboxinput"))).send_keys(sector + ", Islamabad")
+            Submit = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button")
+            Submit.click()
+            sleep(2)
+            Zoom= driver.find_element_by_class_name("ujtHqf-zoom-icon")
+            Zoom.click()
+        
+        
+                      
+        
+
+        if sector== 'G-6':
+            sleep(0.6)
+
+        sleep(0.35)   
+
+        screenshot()
+        green,orange,red,black=image_annalyser()
+        data=data + '<H4>' + emmisions_calculations(green,orange,red,black) + '<H/4> \n'
+        clean_up()
     
-    name=Sector[sectorInitall] + '.png'
-    driver.get_screenshot_as_file(name)
+     
+        WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.ID, "searchboxinput"))).send_keys(Keys.CONTROL + "A")
+        WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.ID, "searchboxinput"))).send_keys(Keys.BACK_SPACE)
+
     driver.quit()
-    print("end...")
-
-
-
-def image_annalyser(): 
-    green,orange,red,black=0,0,0,0
-    from PIL import Image
-    im = Image.open(name, 'r')
-    width, height = im.size
-    pixel_values = list(im.getdata())
-    for i in  pixel_values:  #color has multiple values with small variance, add black color
-        if i==(92, 200, 97, 255)or i== (173, 220, 175, 255):
-            green+=1
-        elif i==(238, 141, 72, 255)or i==(238, 178, 136, 255):
-            orange+=1
-        elif i==(226, 56, 46, 255)or i== (227, 77, 69, 255) or i==(229, 97, 90, 255):
-            red+=1  
-        elif i==(183, 127, 127, 255) or i==(129, 31, 31, 255):
-            black+=1
-    return green,red,orange,black
+    print(data)
+    return data
 
 
 
@@ -56,9 +63,6 @@ def emmisions_calculations(g,o,r,b):
     formula= (555/700) / 6
 
     emmisions=((g*formula*0.2)+(o*formula*0.5)+(r*formula*0.8)+(b*formula*0.96))*0.525*0.2
-    if name[-5]=='5':
-        emmisions/=2
-    #print(str(emmisions))
     emmisions=int(emmisions)
     return str(emmisions)
 
@@ -70,7 +74,33 @@ def clean_up():
     print("File Removed!")
 
 
-    
+
+def screenshot():
+    import pyautogui
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(name)
+
+
+
+def image_annalyser(): 
+    green,orange,red,black=0,0,0,0
+    from PIL import Image
+    im = Image.open(name, 'r')
+    pixel_values = list(im.getdata())
+    for i in  pixel_values:  #color has multiple values with small variance, add black color
+        if i==(93, 201, 97) or i== (204, 230, 205) or i== (160, 218, 163):
+           green+=1
+        elif i==(255, 151, 77)or i==(238, 178, 136):
+           orange+=1
+        elif i==(242, 77, 68)or i== (227, 77, 69) or i==(229, 97, 90):
+           red+=1  
+        elif i==(183, 127, 127) or i==(129, 31, 31):
+           black+=1
+    print(green,red,black,orange)
+    return green,red,orange,black
+
+
+
 def send_to_website(data):
     
     starting_html='''
@@ -125,17 +155,6 @@ def send_to_website(data):
 
 
 
-def process():
-    j=0
-    data=''
-    for i in urls:
-        screenshot(i,j)
-        green,orange,red,black=image_annalyser()
-        data=data +'<h4>' + Sector[j] + ': '+ emmisions_calculations(green,orange,red,black) + ' kg/hour </h4>\n'
-        j+=1
-        clean_up()
-    send_to_website(data)
+send_to_website=website_opener()
 
 
-
-process()
